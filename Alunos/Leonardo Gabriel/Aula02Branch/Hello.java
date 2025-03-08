20e8b6ce132a3d364089316f4d9186cacc8d4c26
Leonardo <gabrielleonardo2512@gmail.com>
2025-03-07 21:42:05 -0300

    Adicionado metodo e trocado Branch

--- Alunos/Leonardo Gabriel/Aula02Branch/Hello.java
+++ Alunos/Leonardo Gabriel/Aula02Branch/Hello.java
@@ -1,5 +1,15 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        olaMundo();
+        holaQueTal();
+    }
+
+    public static void olaMundo() {
+        System.out.println("Olá Mundo");
+    }
+
+    public static void holaQueTal() {
+        System.out.println("Hola que tal");
     }
 }
